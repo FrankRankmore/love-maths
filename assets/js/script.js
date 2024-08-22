fe7bcc207328062c4d74355dbe52cf2a4cf9ad78
Frank Rankmore <franko4000@gmail.com>
2024-08-22 18:49:29 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorroectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswers() {
+
+}
+
+function displayAdditionQuestions() {
+
+}
+
+function displaySubtractQuestions() {
+    
+}
+
+function displayMultiplyQuestions() {
+    
+}
